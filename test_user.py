92d621f9cee455474a19518faabd4b2d8147d04d
AdnanK1 <adnangitonga@gmail.com>
2022-04-23 13:13:43 +0300

    Created a function to test if object has the stored values.

--- test_user.py
+++ test_user.py
@@ -5,4 +5,11 @@ class TestUser(unittest.TestCase):
     
     def setUp(self):
        
-       self.user_detail = User('adnang720@gmail.com','2ba29s') # created user_detail object
+       self.new_user = User('adnang720@gmail.com','2ba29s') # created user_detail object
+    
+    def test_inti(self):
+        self.assertEqual(self.new_user.email,'adnang720@gmail.com')
+        self.assertEqual(self.new_user.password,'2ba29s')
+
+if __name__ == '__main__':
+    unittest.main()
