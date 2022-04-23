3633e6da2eb3d6e17173ede8104bf0e89228c22c
AdnanK1 <adnangitonga@gmail.com>
2022-04-23 13:43:14 +0300

    Added the tearDown function.

--- test_user.py
+++ test_user.py
@@ -4,8 +4,11 @@ from user import User
 class TestUser(unittest.TestCase):
     
     def setUp(self):
-            self.new_user = User('adnang720@gmail.com','2ba29s') # created user_detail object
+        self.new_user = User('adnang720@gmail.com','2ba29s') # created user_detail object
     
+    def tearDown(self):
+        User.user_detail = [] # Resets object
+
     def test_init(self):
         self.assertEqual(self.new_user.email,'adnang720@gmail.com')
         self.assertEqual(self.new_user.password,'2ba29s')
@@ -13,7 +16,8 @@ class TestUser(unittest.TestCase):
     def test_save_user_details(self):
         self.new_user.save_user() 
         self.assertEqual(len(User.user_detail),1)
-        
+
+       
 
 if __name__ == '__main__':
     unittest.main()
