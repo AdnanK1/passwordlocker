6e1b82228c5c7665ef222ddc2c728949a79b2e28
AdnanK1 <adnangitonga@gmail.com>
2022-04-24 09:58:57 +0300

    Created tearDown function to reset object.

--- test_credentials.py
+++ test_credentials.py
@@ -4,4 +4,7 @@ from credentials import Credentials
 class TestCredentials(unittest.TestCase):
     
     def setUp(self):
-        self.credential_details = Credentials('Instagram','kibe@gmail.com','273904') # created user_detail object 
+        self.credential_details = Credentials('Instagram','kibe@gmail.com','273904') # created credential_detail object 
+    
+    def tearDown(self) -> None:
+        Credentials.credential_details = [] #Resets Object
