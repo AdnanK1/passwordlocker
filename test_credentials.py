6a62aebc7a0b8611ae0cd610f500ec6a9cb207c4
AdnanK1 <adnangitonga@gmail.com>
2022-04-24 10:08:22 +0300

    Created test to confirm data is being stored in the object made.

--- test_credentials.py
+++ test_credentials.py
@@ -6,5 +6,10 @@ class TestCredentials(unittest.TestCase):
     def setUp(self):
         self.credential_details = Credentials('Instagram','kibe@gmail.com','273904') # created credential_detail object 
     
-    def tearDown(self) -> None:
+    def tearDown(self):
         Credentials.credential_details = [] #Resets Object
+
+    def test_init(self):
+        self.assertEqual(self.credential_details.name, 'Instagram')
+        self.assertEqual(self.credential_details.email,'kibe@gmail.com')
+        self.assertEqual(self.credential_details.password,'273904')
