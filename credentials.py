692d9c5881cce002a3b824b0a7ba14ef52656d69
AdnanK1 <adnangitonga@gmail.com>
2022-04-24 09:56:21 +0300

    created credential_detail object.

--- credentials.py
+++ credentials.py
@@ -1,2 +1,14 @@
+from user import User
 class Credentials:
-    pass
+    
+    credential_details = [] #empty credential_details list
+
+    def save_credentials(self):
+
+        Credentials.credential_details.append(self)
+
+    def __init__(self,name,email,password):
+        # docstring removed for simplicity
+        self.name = name
+        self.email = email
+        self.password = password
--- test_credentials.py
+++ test_credentials.py
@@ -1,1 +1,7 @@
 import unittest
+from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+    
+    def setUp(self):
+        self.credential_details = Credentials('Instagram','kibe@gmail.com','273904') # created user_detail object 
